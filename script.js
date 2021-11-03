fbb8d81cad622d203c17978160750efd789c8369
Glitch (ghost-11-3project) <none>
2021-11-03 22:26:49 +0000

    👴🌂 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+$(".hide-button").click(function()
+{$(".ghost-image").hide()});
+
+$(".show-button").click(function() 
+ {$(".ghost-image").show()});
+
+$(".message-button").click(function() 
+ {$(".main").append()});
 
